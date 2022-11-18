2b4cd0da642a6eb89090d4ba78bd94f94d02abab
MostafaDe <mostafa00734@gmail.com>
2022-11-18 21:51:43 +0330

    Update download_data function
    Supporting two more data centers(IRIS,ISC)

--- EarthquakeForecast/data_downloading/download_data.py
+++ EarthquakeForecast/data_downloading/download_data.py
@@ -7,8 +7,8 @@ def input_checking_download_data(time_interval,save_dataframe,magnitude_range,de
     if not isinstance(data_center,str):
         raise TypeError(f'Error: invalid type for \'data_center\'. expected type is str, got {type(data_center)}')
     # value checking:
-    if not (data_center in ['USGS']):
-        raise ValueError('Error: invalid value for data_center. in this version only \'USGS\' is available')
+    if not (data_center in ['USGS','IRIS','ISC']):
+        raise ValueError('Error: invalid value for data_center. in this version we support [\'USGS\',\'IRIS\',\'ISC\'].')
     
     # checking 'time_interval' input:
      # type checking 1:
@@ -21,7 +21,7 @@ def input_checking_download_data(time_interval,save_dataframe,magnitude_range,de
     if not (isinstance(time_interval[0],str) and isinstance(time_interval[1],str)):
         raise TypeError('Error: invalid type for \'time_interval\'. expected all elements to be str type.')
     # value checking:
-    r1,r2 = [datetime.strptime(r,'%Y/%m/%d') for r in time_interval]
+    r1,r2 = [datetime.strptime(r,'%Y-%m-%d') for r in time_interval]
     if not (r1 < r2):
         raise ValueError('Error: invalid value for \'time_interval\'. expected first element to be less than' +
         ' second element')
@@ -51,17 +51,27 @@ def input_checking_download_data(time_interval,save_dataframe,magnitude_range,de
         if not isinstance(circular_region,dict):
             raise TypeError(f'Error: invalid type for \'circular_region\'.expected type is dict, got {type(circular_region)}.')
         # key checking:
-        if not (('radius' in circular_region.keys()) and ('longitude' in circular_region.keys()) and ('latitude' in circular_region.keys())):
-            raise KeyError('Error: invalid keys for \'circular_region\'. expected *radius*, *longitude* and *latitude* keys'+
-            ' to be in the dictionary keys.')
+        if data_center == 'USGS':
+          if not (('radius' in circular_region.keys()) and ('longitude' in circular_region.keys()) and ('latitude' in circular_region.keys())):
+              raise KeyError('Error: invalid keys for \'circular_region\'. expected *radius*, *longitude* and *latitude* keys'+
+              ' to be in the dictionary keys.')
+        else:
+          if not (('maxradius' in circular_region.keys()) and ('minradius' in circular_region.keys())  and ('longitude' in circular_region.keys()) and ('latitude' in circular_region.keys())):
+              raise KeyError('Error: invalid keys for \'circular_region\'. expected *radius*, *longitude* and *latitude* keys'+
+              ' to be in the dictionary keys.')          
         # value checking:
         if not ((-180 <= circular_region['longitude'] <= 180) and (-90 <= circular_region['latitude'] <= 90)):
             raise ValueError('Error: invalid value for \'circular_region\'.expected values are:\n'+
                              '-180 <= longitude <= +180, -90 <= latitude <= 90.')
-        if not (0 < circular_region['radius']):
-            raise ValueError('Error: invalid value for \'circular_region\'.expected value is:\n'+
-                             '0 < radius.')
-    
+        if data_center == 'USGS':
+          if not (0 < circular_region['radius']):
+              raise ValueError('Error: invalid value for \'circular_region\'.expected value is:\n'+
+                              '0 < minradius < 180, 0 < maxradius <= 180.')
+        else:
+          if not ((0 < circular_region['minradius'] < 180) or (0 < circular_region['maxradius'] <= 180)):
+              raise ValueError('Error: invalid value for \'circular_region\'.expected value is:\n'+
+                              '0 < minradius < 180, 0 < maxradius <= 180.')
+      
     # either 'circular_region' or 'rectangular_region' must be initiated.
     if (circular_region is None) and (rectangular_region is None):
         raise Exception('Error: either \'circular_region\' or \'rectangular_region\' must be initiated.')
@@ -102,6 +112,7 @@ def input_checking_download_data(time_interval,save_dataframe,magnitude_range,de
     # type checking:
     if not isinstance(save_dataframe,bool):
         raise TypeError(f'Error: invalid type for \'save_address\'. expected type is bool, got {type(save_dataframe)}')
+    
 
 
 
@@ -111,26 +122,44 @@ def download_data(time_interval,save_dataframe = False,magnitude_range=None,dept
        time interval and the geographical locations defined by user input'''
     from urllib.request import urlopen
     import pandas as pd
-
+    from bs4 import BeautifulSoup
     input_checking_download_data(time_interval,save_dataframe,magnitude_range,depth_range,data_center,
                                 rectangular_region,circular_region)
 
     if data_center == 'USGS':
         url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv'
-        url = url + '&starttime=' + time_interval[0] + '&endtime=' + time_interval[1]
-        if magnitude_range is not None:
-            url = url + '&minmagnitude=' + str(magnitude_range[0]) + '&maxmagnitude=' + str(magnitude_range[1])
-        if depth_range is not None:
-            url = url + '&mindepth=' + str(depth_range[0]) + '&maxdepth=' + str(depth_range[1])
-        if rectangular_region is not None:
-            url = url + '&minlatitude=' + str(rectangular_region[0]) + '&maxlatitude=' + str(rectangular_region[1]) + '&minlongitude=' + str(rectangular_region[2]) + \
-                    '&maxlongitude=' + str(rectangular_region[3])
-        elif circular_region is not None:
-            url = url + '&maxradiuskm=' + str(circular_region['radius'])+ '&longitude=' +str( circular_region['longitude']) + \
-                 '&latitude=' + str(circular_region['latitude'])
-
-        csv_url_rect = urlopen(url)
-        downloaded_data = pd.read_csv(csv_url_rect)
-        if save_dataframe:
-           downloaded_data.to_csv('earthquake_data.csv')
-        return downloaded_data
+    elif data_center == 'ISC':
+        url = 'http://isc-mirror.iris.washington.edu/fdsnws/event/1/query?format=xml'
+    elif data_center == 'IRIS':
+        url = 'http://service.iris.edu/fdsnws/event/1/query?format=xml'
+    url = url + '&starttime=' + time_interval[0] + '&endtime=' + time_interval[1]
+    if magnitude_range is not None:
+        url = url + '&minmagnitude=' + str(magnitude_range[0]) + '&maxmagnitude=' + str(magnitude_range[1])
+    if depth_range is not None:
+        url = url + '&mindepth=' + str(depth_range[0]) + '&maxdepth=' + str(depth_range[1])
+    if rectangular_region is not None:
+        url = url + '&minlatitude=' + str(rectangular_region[0]) + '&maxlatitude=' + str(rectangular_region[1]) + '&minlongitude=' + str(rectangular_region[2]) + \
+                '&maxlongitude=' + str(rectangular_region[3])
+    elif circular_region is not None:
+        url = url + '&longitude=' +str( circular_region['longitude']) + \
+                    '&latitude=' + str(circular_region['latitude'])
+        if data_center == 'USGS':
+            url = url + '&maxradiuskm=' + str(circular_region['radius'])
+        if data_center == 'ISC' or data_center == 'IRIS':
+            url = url + '&maxradius=' + str(circular_region['maxradius']) + '&minradius=' + str(circular_region['minradius'])
+    downloaded_url = urlopen(url)
+    if data_center == 'USGS':
+        downloaded_data = pd.read_csv(downloaded_url)
+    else:
+        bs_data = BeautifulSoup(downloaded_url, "xml")
+        events = bs_data.find_all('event')
+        downloaded_data = pd.DataFrame(columns=['time','latitude','longitude','depth','magnitude'])
+        for event in events:
+            downloaded_data = downloaded_data.append({'time':event.find('time').find('value').text,
+                        'latitude':float(event.find('latitude').find('value').text) ,
+                        'longitude':float(event.find('longitude').find('value').text),
+                        'depth':float(event.find('depth').find('value').text),
+                        'magnitude':float(event.find('mag').find('value').text)},ignore_index=True)
+    if save_dataframe:
+        downloaded_data.to_csv('earthquake_data.csv')
+    return downloaded_data
